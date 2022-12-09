90257309b65065a7ac2455028439e80df0da4372
Strabatras <strabatras@gmail.com>
2022-12-09 15:37:17 +0300

    work-002 : приложение переведено на Annotation + Java Based конфигурацию #002

--- homework/work-002/src/main/java/ru/otus/homework/dao/impl/QuizDaoImpl.java
+++ homework/work-002/src/main/java/ru/otus/homework/dao/impl/QuizDaoImpl.java
@@ -1,6 +1,7 @@
 package ru.otus.homework.dao.impl;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 import ru.otus.homework.dao.QuizDao;
 import ru.otus.homework.domain.Quiz;
 import ru.otus.homework.domain.QuizAnswer;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
+@Component
 public class QuizDaoImpl implements QuizDao {
     private final DataReader dataReader;
 
