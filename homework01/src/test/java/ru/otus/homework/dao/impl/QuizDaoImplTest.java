887b0be60c01765e0917235747709a421c4dcd3f
Strabatras <strabatras@gmail.com>
2023-01-15 16:36:54 +0300

    work-001 : директория проекта изменена на homework-001 #001

---
+++ homework01/src/test/java/ru/otus/homework/dao/impl/QuizDaoImplTest.java
@@ -0,0 +1,55 @@
+package ru.otus.homework.dao.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.homework.domain.Quiz;
+import ru.otus.homework.util.DataReader;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+import static ru.otus.homework.DataFactory.CLASS_SIMPLE_NAME_QUIZ;
+import static ru.otus.homework.DataFactory.CLASS_SIMPLE_NAME_QUIZ_ANSWER;
+import static ru.otus.homework.DataFactory.preparedLinesFromFileWithEmptyLines;
+
+@DisplayName("Вопросы/ответы - DAO")
+@ExtendWith(MockitoExtension.class)
+class QuizDaoImplTest {
+    @Mock
+    private DataReader dataReader;
+    @InjectMocks
+    private QuizDaoImpl quizDao;
+
+    @DisplayName("возвращается корректный список")
+    @Test
+    void shouldBeCreateCorrectQuizzes() {
+        final List<List<String>> dataToCheck = preparedLinesFromFileWithEmptyLines();
+        when(dataReader.readLines()).thenReturn(dataToCheck);
+        final List<Quiz> quizzes = quizDao.getQuizzes();
+        assertNotNull(quizzes);
+
+        final String[] quizzesNames = dataToCheck.stream()
+                .filter(line -> line.stream().findFirst().orElse("").trim().length() > 0)
+                .map(line -> line.get(0).trim())
+                .toArray(String[]::new);
+
+        assertEquals(dataToCheck.size() - (dataToCheck.size() - quizzesNames.length), quizzes.size());
+
+        for (int i = 0; i < quizzesNames.length; i++) {
+            var quiz = quizzes.get(i);
+            assertEquals(CLASS_SIMPLE_NAME_QUIZ, quiz.getClass().getSimpleName());
+            assertEquals(quizzesNames[i], quiz.getName());
+            for (int j = 0; j < quiz.getAnswers().size(); j++) {
+                var quizAnswer = quiz.getAnswers().get(j);
+                assertEquals(CLASS_SIMPLE_NAME_QUIZ_ANSWER, quizAnswer.getClass().getSimpleName());
+                assertEquals(quizzesNames[i] + (j + 1), quizAnswer.getName());
+            }
+        }
+    }
+}
---
+++ homework01/src/test/java/ru/otus/homework/service/impl/QuizServiceImplTest.java
@@ -0,0 +1,33 @@
+package ru.otus.homework.service.impl;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import ru.otus.homework.dao.QuizDao;
+import ru.otus.homework.domain.Quiz;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static ru.otus.homework.DataFactory.quizzesWithAnswers;
+
+@DisplayName("Сервис вопросов")
+@ExtendWith(MockitoExtension.class)
+class QuizServiceImplTest {
+    @Mock
+    private QuizDao quizDao;
+    @InjectMocks
+    private QuizServiceImpl quizService;
+
+    @DisplayName("корректно возвращает список вопросов/ответов")
+    @Test
+    void shouldReturnCorrectQuizzesList() {
+        final List<Quiz> quizzes = quizzesWithAnswers();
+        when(quizDao.getQuizzes()).thenReturn(quizzes);
+        assertEquals(quizzes, quizService.getQuizzes());
+    }
+}
